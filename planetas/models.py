e2f66c7a4b8ae0036b818a553d45341a471626fa
Marcos <marcos.pedroo@gmail.com>
2020-11-02 23:05:22 -0300

    Ajuste nos campos Clima e Terreno para que suas opções sejam mais limitadas

--- planetas/models.py
+++ planetas/models.py
@@ -6,9 +6,11 @@ class Planeta(models.Model):
     class Meta:
         db_table = 'planeta'
 
-    nome = models.CharField(max_length=200)
-    clima = models.CharField(max_length=200)
-    terreno = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200, unique=True)
+    clima = models.CharField(max_length=2, choices=[('CO', 'Congelado'), ('TR', 'Tropical'), ('TE', 'Temperado'),
+                                                                                                ('AR','Árido')])
+    terreno = models.CharField(max_length=200, choices=[('DE', 'Deserto'), ('OC', 'Oceano'), ('FL', 'Florestas'),
+                                                        ('MO', 'Montanhoso'), ('TU', 'Tundra'), ('PA', 'Pantano')])
 
     def __str__(self):
-        return self.title
+        return self.nome
