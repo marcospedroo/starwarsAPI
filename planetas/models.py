799a35648e635c55c711391547167df99eb5c68d
Marcos <marcos.pedroo@gmail.com>
2020-11-02 23:05:51 -0300

    ajuste nos espaços

--- planetas/models.py
+++ planetas/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class Planeta(models.Model):
-
     class Meta:
         db_table = 'planeta'
 
     nome = models.CharField(max_length=200, unique=True)
     clima = models.CharField(max_length=2, choices=[('CO', 'Congelado'), ('TR', 'Tropical'), ('TE', 'Temperado'),
-                                                                                                ('AR','Árido')])
+                                                    ('AR', 'Árido')])
     terreno = models.CharField(max_length=200, choices=[('DE', 'Deserto'), ('OC', 'Oceano'), ('FL', 'Florestas'),
                                                         ('MO', 'Montanhoso'), ('TU', 'Tundra'), ('PA', 'Pantano')])
 
